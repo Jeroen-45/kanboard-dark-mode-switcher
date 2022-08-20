897f22f0f0b326724f975b7f853aa64fa25eb92a
Jeroen Tas <jeroentas@live.nl>
2022-08-20 22:13:57 +0200

    Add switcher button to user dropdown

---
+++ Template/SwitcherButton.php
@@ -0,0 +1,12 @@
+<?php
+if ($this->user->userMetadataModel->get($this->user->getid(), "DarkModeSwitcher_DarkMode", false)) : ?>
+<li>
+    <i class="fa fa-fw fa-moon-o" style="display: inline-block"></i>
+    <a href="#">Switch to dark mode</a>
+</li>
+<?php else : ?>
+<li>
+    <i class="fa fa-fw fa-sun-o" style="display: inline-block"></i>
+    <a href="#">Switch to light mode</a>
+</li>
+<?php endif; ?>
